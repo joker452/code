9ef4a1b7ff102f36c4987c3d91df29bddaa4adc7
joker452 <dbw15@mails.tsinghua.edu.cn>
2018-12-11 17:38:33 +0800

    add save file but with different size

--- python/semi-supervised/gui.py
+++ python/semi-supervised/gui.py
@@ -2,7 +2,9 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, \
     QFileDialog, QLabel, QInputDialog
 from PyQt5.QtWidgets import QDesktopWidget, QMessageBox
-from PyQt5.QtGui import QPixmap, QKeySequence, QIcon, QCloseEvent
+from PyQt5.QtGui import QPixmap, QKeySequence, QIcon, QCloseEvent,\
+    QImage
+from PyQt5.QtCore import QFile, QIODevice, QBuffer,QByteArray, QSaveFile
 
 
 # TODO: zoom in, zoom out
@@ -23,7 +25,7 @@ class MainWindow(QMainWindow):
         self.addMenu()
         # add toolbar
         self.addToolbar()
-        self.pixmap = self.filename = None
+        self.pixmap = self.filename = self.image = self.file = None
         # for save prompt
         self.modified = False
         self.layout()
@@ -40,8 +42,9 @@ class MainWindow(QMainWindow):
         saveAction.setShortcut(QKeySequence('Ctrl+S'))
         saveAction.setStatusTip('open an image file')
         saveasAction = QAction('Save As', fileMenu)
-        saveAction.setShortcut('Ctrl+A')
+        saveAction.setShortcut(QKeySequence('Ctrl+A'))
         openAction.triggered.connect(self.openFile)
+        saveasAction.triggered.connect(self.saveasFile)
 
         fileMenu.addAction(openAction)
         fileMenu.addAction(saveAction)
@@ -63,16 +66,32 @@ class MainWindow(QMainWindow):
         if self.checkSave():
             self.label = QLabel(self)
             self.label.setScaledContents(True)
-            self.setCentralWidget(self.label)
-            self.filename, _ = QFileDialog.getOpenFileName(filter="Images (*.png *.jpg)")
-            self.pixmap = QPixmap(self.filename)
+            self.filename, _ = QFileDialog.getOpenFileName(caption="Open an image",
+                                                           filter="JPEG (*.jpeg *.jpg);;PNG (*.png)")
+            if self.filename:
+                self.file = QFile(self.filename)
+                if not self.file.open(QIODevice.ReadOnly):
+                    return False
+                self.imageData = self.file.readAll()
+                if self.imageData.isEmpty():
+                    return False
+                self.file.close()
 
-            self.frameGeometry().size()
-            self.resize(self.pixmap.size())
-            self.label.setPixmap(self.pixmap)
-            self.qbsAction.setEnabled(True)
-            self.qbeAction.setEnabled(True)
-            self.movetoCenter()
+                self.image = QImage()
+                self.buffer = QBuffer(self.imageData)
+                self.buffer.open(QIODevice.ReadOnly)
+                self.image.load(self.buffer, "jpg")
+                self.buffer.close()
+                self.setCentralWidget(self.label)
+
+                self.pixmap = QPixmap.fromImage(self.image)
+                self.frameGeometry().size()
+
+                self.resize(self.pixmap.size())
+                self.label.setPixmap(self.pixmap)
+                self.qbsAction.setEnabled(True)
+                self.qbeAction.setEnabled(True)
+                self.movetoCenter()
 
     def search(self):
         text, ok = QInputDialog.getText(self, 'Input', "Enter")
@@ -87,6 +106,36 @@ class MainWindow(QMainWindow):
         if self.pixmap.save(self.filename, fileFormat):
             self.modified = False
 
+    def saveasFile(self):
+        savefile, filter = QFileDialog.getSaveFileName(caption="Save as", directory='./no_title.jpeg',
+                                               filter="JPEG (*.jpeg, *.jpg);;PNG (*.png);;BMP (*.bmp)")
+        print(self.image)
+        if self.image:
+            # print(savefile)
+            # savefile = savefile.lower()
+            # if filter.startswith('JPEG'):
+            #     if not savefile.endswith('jpeg') and not savefile.endswith('jpg'):
+            #         savefile += '.jpeg'
+            # elif filter.startswith('PNG'):
+            #     if not savefile.endswith('png'):
+            #         savefile += '.png'
+            # else:
+            #     if not savefile.endswith('bmp'):
+            #         savefile += '.bmp'
+            # self.image.save(savefile, quality=100)
+            self.imageData = QByteArray()
+            self.buffer = QBuffer(self.imageData)
+            self.buffer.open(QIODevice.WriteOnly)
+            self.image.save(self.buffer)
+            print(self.buffer.size())
+            file = QSaveFile(savefile)
+            if not file.open(QIODevice.WriteOnly):
+                print("No")
+                return False
+            print(self.imageData.size())
+            file.write(self.imageData)
+
+
     def checkSave(self):
         if self.modified:
             reply = QMessageBox.warning(self, "Test",
@@ -98,7 +147,7 @@ class MainWindow(QMainWindow):
                 return self.saveFile()
             elif reply == QMessageBox.Cancel:
                 return False
-            return True
+        return True
 
     def closeEvent(self, e: QCloseEvent):
         if self.checkSave():
