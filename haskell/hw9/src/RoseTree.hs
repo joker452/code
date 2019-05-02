95ab12969b3f64106f3ed6a018e2bdcd2c604374
joker452 <dbw15@mails.tsinghua.edu.cn>
2019-05-02 15:16:59 +0800

    finish hw9

--- haskell/hw9/src/RoseTree.hs
+++ haskell/hw9/src/RoseTree.hs
@@ -3,8 +3,7 @@ module RoseTree where
 import RoseTreeType
 
 mapRoseTree :: (a -> b) -> RoseTree a -> RoseTree b
-mapRoseTree f (Node a nodes) = case nodes of
-                                            [] -> Node (f a) []
-                                            xs -> Node (f a) (map (mapRoseTree f) xs)
+mapRoseTree f (Node a nodes) = case nodes of [] -> Node (f a) []
+                                             xs -> Node (f a) (map (mapRoseTree f) xs)
 
 instance Functor RoseTree where fmap = mapRoseTree
--- haskell/hw9/test/Spec.hs
+++ haskell/hw9/test/Spec.hs
@@ -1,17 +1,4 @@
-
-import RoseTreeType
-import RoseTree
-import TriangleType
-import Trans
-
-tree = Node 1 [Node 2 [Node 3 [], Node 4[]], Node 5 []]
-a = Point 1 2
-b = Point 2 3
-c = Point 3 4
-t = Triangle a b c
-
 main :: IO ()
---main = print $ fmap id tree
---       print $ fmap ((+1) . (+2)) tree == Node 4 [Node 5 [Node 6 [], Node 7[]], Node 8 []]
---       print $ fmap (+1) . fmap (+2) $ tree == Node 4 [Node 5 [Node 6 [], Node 7 []], Node 8 []]
-main = undefined
+main = do
+       print $ True == True
+       print $ False == False
